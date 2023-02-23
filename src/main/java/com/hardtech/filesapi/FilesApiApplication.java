55a199b0056288a833057afe279ca81625aea006
TESSI Hardson <hardsontessi2@gmail.com>
2023-02-23 15:44:02 +0100

    H2 migration

--- src/main/java/com/hardtech/filesapi/FilesApiApplication.java
+++ src/main/java/com/hardtech/filesapi/FilesApiApplication.java
@@ -1,12 +1,23 @@
 package com.hardtech.filesapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(title = "Files API", version = "1.0.0"),
+        servers = {@Server(url = "https://files-api-p9ni.onrender.com")},
+        tags = {@Tag(name = "Files API", description = "This is the files API..")}
+)
 public class FilesApiApplication {
 
     public static void main(String[] args) {
